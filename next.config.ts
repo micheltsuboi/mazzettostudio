589feafea644c5f723a32ec9805b46efd3f9dfd1
Michel Tsuboi <micheltsuboi@MacBook-Air-de-Michel.local>
2026-03-19 13:59:13 -0300

    update supabase project id for images

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cclopaqksolrcytgxdia.supabase.co',
+        hostname: 'xdrwdcnvpueikqdhaova.supabase.co',
       },
     ],
   },
